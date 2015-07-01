3d714af5dd870a70f7d5d1417f3378b864addd18
AlexV <asmodehn@gmail.com>
2015-07-01 16:51:46 +0900

    Imported upstream version '0.0.3' of 'upstream'

---
+++ src/rosinterface/__init__.py
@@ -0,0 +1,6 @@
+#This python package is handling all ROS related communication for rostful-node.
+from __future__ import absolute_import
+
+from .topic import TopicBack
+from .action import ActionBack
+from .service import ServiceBack
